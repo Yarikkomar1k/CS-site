bb6f0383024de596be0725ae304d6baca340514f
Yarikkomar1k <tishkamurtishka@gmail.com>
2026-03-29 19:41:45 +0300

    +

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const video = document.getElementById("introVideo");
+const skipBtn = document.getElementById("skipBtn");
 
+const videoScreen = document.getElementById("video-screen");
+const mainSite = document.getElementById("main-site");
+
+skipBtn.onclick = startSite;
+
+video.onended = startSite;
+
+function startSite(){
+    videoScreen.style.display = "none";
+    mainSite.style.display = "block";
+    showPage("maps");
+}
+
+function showPage(pageId){
+
+    const pages = document.querySelectorAll(".page");
+
+    pages.forEach(page=>{
+        page.style.display="none";
+    });
+
+    document.getElementById(pageId).style.display="block";
+}
